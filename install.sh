f6db46a2e6ab2f76e8057e61607cee7c2cf7538a
Duncan <togs@protonmail.com>
2020-02-26 19:49:35 -0500

    fixed symlinks

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="exports gitconfig bashrc vimrc bash_profile functions alias inputrc prompt"    # list of files/folders to symlink in homedir
 
 ##########
 
